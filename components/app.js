c86e311ff77ce9112df55def16f6e2711514b98e
gleep gloop <jesskawazoe@gmail.com>
2020-08-29 14:43:50 -0700

    keeping getting errors

--- components/app.js
+++ components/app.js
@@ -1,9 +1,14 @@
 class App {
   constructor(gradeTable, pageHeader) {
     this.gradeTable = gradeTable;
-    this.handleGetGradesError = this.handleGetGradesError.bind(this);
-    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+    // this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    // this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
     this.pageHeader = pageHeader;
+    this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
+    this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
+    this.deleteGrade = this.deleteGrade.bind(this);
+    this.handleDeleteGradeSuccess = this.handleDeleteGradeSuccess.bind(this);
+    this.handleDeleteGradeError = this.handleDeleteGradeError.bind(this);
   }
 
   handleGetGradesError(error) {
@@ -15,6 +20,7 @@ class App {
 
     var total = 0;
     for (var gradeList = 0; gradeList < grades.length; gradeList++) {
+      parseInt(grades[gradeList].grade, 10);
       total += grades[gradeList].grade;
     }
     var averageOfGrades = total / (grades.length);
@@ -33,6 +39,50 @@ class App {
   }
 
   start() {
+    this.getGrades();
+    this.gradeForm.onSubmit(this.createGrade);
+    this.gradeTable.onDeleteClick(this.deleteGrade);
+  }
+
+  createGrade(name, course, grade) {
+    // console.log(name, course, grade);
+    $.ajax({
+      type: "POST",
+      headers: { "x-access-token": "lP7bPRsj" },
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      data: {
+        "name": name,
+        "course": course,
+        "grade": grade,
+      },
+      success: this.handleCreateGradeSuccess,
+      error: this.handleCreateGradeError,
+    });
+  }
+
+  handleCreateGradeError(error) {
+    console.error(error);
+  }
+
+  handleCreateGradeSuccess() {
+    this.getGrades();
+  }
+
+  deleteGrade(id) {
+    $.ajax({
+      method: "DELETE",
+      headers: { "x-access-token": "lP7bPRsj" },
+      url: "https://sgt.lfzprototypes.com/api/grades/" + id,
+      success: this.handleDeleteGradeSuccess,
+      error: this.handleDeleteGradeError
+    });
+  }
+
+  handleDeleteGradeError(error) {
+    console.error(error);
+  }
+
+  handleDeleteGradeSuccess() {
     this.getGrades();
   }
 }
--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 // Before App is instantiated, query the document for your table element.
 var table = document.querySelector("table");
 var header = document.querySelector("header");
+var form = document.querySelector("form");
+var pTag = document.getElementsByClassName("pTag");
+
+var gradeForm = new GradeForm(form);
 
 // After you get the table element, but before instantiating App, instantiate GradeTable, passing in the table element.
-var gradeTable = new GradeTable(table);
+//var gradeTable = new GradeTable(table);
+var gradeTable = new GradeTable(table, pTag);
 
 // Before App is instantiated, instantiate PageHeader
 var pageHeader = new PageHeader(header);
 
 // Modify your instantiation of App to pass in your GradeTable instance as an argument.
-var app = new App(gradeTable, pageHeader);
+var app = new App(gradeTable, pageHeader, gradeForm);
 app.start();
